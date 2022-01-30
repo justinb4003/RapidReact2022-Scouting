b5f9c4e83f6fa5315b2345e15ad8a19a059cfda5
Justin Buist <justin.buist@gmail.com>
2022-01-29 20:58:43 -0500

    Set up basic routing

--- trisonics-scouting/src/app/app-routing.module.ts
+++ trisonics-scouting/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewResultsComponent } from './components/view-results/view-results.component';
+import { ScoreMatchComponent } from './components/score-match/score-match.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "score-match",
+    component: ScoreMatchComponent,
+  },
+  {
+    path: "view-results",
+    component: ViewResultsComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
