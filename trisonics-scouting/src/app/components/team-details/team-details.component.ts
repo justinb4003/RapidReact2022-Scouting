994f964c3c79c2fc0b1685eb0937c9eb7fc46641
Justin Buist <justin.buist@gmail.com>
2022-03-11 19:01:56 -0500

    Adds CSV export

--- trisonics-scouting/src/app/components/team-details/team-details.component.ts
+++ trisonics-scouting/src/app/components/team-details/team-details.component.ts
@@ -109,4 +109,30 @@ export class TeamDetailsComponent implements OnInit, AfterViewInit {
     });
 
   }
+
+  public downloadFile(data: any) {
+    const replacer = (key: any, value: any) => (value === null ? '' : value); // specify how you want to handle null values here
+    const header = Object.keys(data[0]);
+    const csv = data.map((row: any) =>
+      header
+        .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+        .join(',')
+    );
+    csv.unshift(header.join(','));
+    const csvArray = csv.join('\r\n');
+
+    const a = document.createElement('a');
+    const blob = new Blob([csvArray], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+
+    a.href = url;
+    a.download = 'opr-calcs.csv';
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  }
+
+  public exportCSV(): void {
+    this.downloadFile(this.oprData.data);
+  }
 }
--- trisonics-scouting/src/app/components/view-results/view-results.component.ts
+++ trisonics-scouting/src/app/components/view-results/view-results.component.ts
@@ -146,4 +146,30 @@ export class ViewResultsComponent implements OnInit, AfterViewInit {
       this.dataLoading = false;
     });
   }
+
+  public downloadFile(data: any) {
+    const replacer = (key: any, value: any) => (value === null ? '' : value); // specify how you want to handle null values here
+    const header = Object.keys(data[0]);
+    const csv = data.map((row: any) =>
+      header
+        .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+        .join(',')
+    );
+    csv.unshift(header.join(','));
+    const csvArray = csv.join('\r\n');
+
+    const a = document.createElement('a');
+    const blob = new Blob([csvArray], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+
+    a.href = url;
+    a.download = 'scout-data.csv';
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  }
+
+  public exportCSV(): void {
+    this.downloadFile(this.scoutData.data);
+  }
 }
